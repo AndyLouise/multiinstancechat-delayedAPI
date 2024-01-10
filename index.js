a07449d28c680ce51ae66a134f69d3dbc0ed9dda
andorelouise@gmail.com <andrepereira180903@gmail.com>
2024-01-10 01:08:13 +0000

    try catch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = 3000;
-const delayTimeInMinutes = 4; // 4 minutes delay 
+const delayTimeInMinutes = 5; // 4 minutes delay 
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -40,37 +40,41 @@ const processQueue = () => {
             });
             console.log(`Executed call to delete players: ${request.number}`);
             processQueue(); // Process next request
-        }, delayTimeInMinutes * 60 * 1000); // 4 minutes delay 4 * 60 * 1000
+        }, delayTimeInMinutes * 60 * 1000); // 5 minutes delay 5 * 60 * 1000
     }
 };
 
-
-app.get("/", async (req, res) => {
-  const alive = req.query.alive || 1;
-
-  if (alive == 1) {
-    res.json({Status: "Alive"});
-    return;
-  }
-});
-
-// API endpoint to receive requests
-app.get('/enqueue', async (req, res) => {
-  try {
-    const number = req.query.n || null;
-    if(number == null) {
-      res.end("Bad Request");
+try {
+  app.get("/", async (req, res) => {
+    const alive = req.query.alive || 1;
+  
+    if (alive == 1) {
+      res.json({Status: "Alive"});
       return;
     }
-    requestQueue.push({ number });
-    console.log(`Enqueued player removal: ${number}`);
-    processQueue();
-    res.json({ message: 'Request enqueued.' });
-  } catch(error) {
-    console.log("Error:", error)
-  }
-});
+  });
+  
+  // API endpoint to receive requests
+  app.get('/enqueue', async (req, res) => {
+    try {
+      const number = req.query.n || null;
+      if(number == null) {
+        res.end("Bad Request");
+        return;
+      }
+      requestQueue.push({ number });
+      console.log(`Enqueued player removal: ${number}`);
+      processQueue();
+      res.json({ message: 'Request enqueued.' });
+    } catch(error) {
+      console.log("Error:", error)
+    }
+  });
+  
+  app.listen(PORT, () => {
+      console.log(`API server is running on port ${PORT}`);
+  });
+} catch (error) {
+  console.log(error);
+}
 
-app.listen(PORT, () => {
-    console.log(`API server is running on port ${PORT}`);
-});
